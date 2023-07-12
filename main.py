b4cd57ff3d011c78498adea6ccd15cdaa1d7fba5
riguima <richard.alexsander.guima@gmail.com>
2023-07-12 13:11:00 -0300

    change message

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     browser.make_login('craybrasil@gmail.com', 'Projeto#1bot')
     browser.open_inbox()
     message = (
-        '{saudação}, tudo bem? \n'
+        '{saudação}, tudo bem?. '
         'Estamos com esse produto atualmente {b}"pacotedeautomacoes.site"'
         '{/b} caso você possua interesse. Podemos enviar os acessos '
         'através da plataforma {b}99freelas{/b}. Consiste em um pacote com '
